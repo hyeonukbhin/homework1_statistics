7bbe5612c7674a8c2bad2380115dae43918d2957
hyeonukbhin <bhu@kist.re.kr>
2018-10-22 10:32:29 +0900

    complete

--- sample_mean_comparison.py
+++ sample_mean_comparison.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import configparser
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
+import pandas as pd
 import seaborn as sns
-
 
 excel_file = "data.xlsx"
 
@@ -15,20 +11,21 @@ df_all = pd.read_excel(excel_file, index_col=0)
 
 df_population = df_all["데이터"].reset_index(drop=True)
 
-# print(population)
-
 # set parameter
 # N = 10
 Ns = [10, 20, 30, 40, 50]
+
 
 def cal_mean_std(df):
     mean = df.mean()
     std = df.std()
     return mean, std
 
+
 def add_df(index, columns, value, df=None):
     df.loc[index, columns] = value
     return df
+
 
 def draw_box_plot(df, x=[], y=[], title="E vs Subfeature", with_swarmplot=False):
     fig = plt.figure(figsize=(30, 15))
@@ -36,7 +33,6 @@ def draw_box_plot(df, x=[], y=[], title="E vs Subfeature", with_swarmplot=False)
 
     for idx, val in enumerate(x):
         ax = fig.add_subplot("{}{}{}".format(len(x), 1, idx + 1))
-        # ax.set_title("Subfeature {}".format(val), fontsize=16)
         sns.boxplot(x=df[val], y=df[y[idx]], ax=ax)
         ax.grid(color='k', linestyle='-', linewidth=0.5)
         if with_swarmplot is True:
@@ -48,11 +44,11 @@ writer = pd.ExcelWriter("result.xlsx", engine='openpyxl')
 
 for N in Ns:
     df_samples = pd.DataFrame(dtype=float)
-    coll_names = ["sample_1", "sample_2", "sample_3", "sample_4", "sample_5", "sample_6", "sample_7", "sample_8", "sample_9", "sample_10"]
+    coll_names = ["sample_1", "sample_2", "sample_3", "sample_4", "sample_5", "sample_6", "sample_7", "sample_8",
+                  "sample_9", "sample_10"]
 
     for coll_name in coll_names:
         df_samples[coll_name] = df_population.sample(N).reset_index(drop=True)
-
 
     df_sample_mean_std = pd.DataFrame(dtype=float)
     for coll_name in coll_names:
@@ -60,13 +56,10 @@ for N in Ns:
         df_sample_mean_std = add_df(coll_name, "mean", mean, df_sample_mean_std)
         df_sample_mean_std = add_df(coll_name, "std", std, df_sample_mean_std)
 
-
-
     mean, _ = cal_mean_std(df_sample_mean_std["mean"])
     _, std = cal_mean_std(df_sample_mean_std["std"])
     df_mean_std = add_df(N, "mean", mean, df_mean_std)
     df_mean_std = add_df(N, "std", std, df_mean_std)
-
 
     print("========================================")
     print("sample raw data, N = {}".format(N))
@@ -76,7 +69,7 @@ for N in Ns:
     print(df_sample_mean_std)
 
     df_samples.to_excel(writer, index=False, sheet_name="sample_{}".format(N))
-    df_sample_mean_std.to_excel(writer, startcol=df_samples.shape[1]+2, index=False, sheet_name="sample_{}".format(N))
+    df_sample_mean_std.to_excel(writer, startcol=df_samples.shape[1] + 2, index=False, sheet_name="sample_{}".format(N))
 
 print("========================================")
 print("mean, std of sample's means According to Ns".format(N))
@@ -84,22 +77,9 @@ print(df_mean_std)
 df_mean_std.to_excel(writer, index=False, sheet_name="total")
 writer.save()
 
-# df_mean_std.to_excel("result.xlsx", sheet_name="Sheet1")
-# df_samples.to_excel("result.xlsx", sheet_name="Sheet1")
-
-# print(df_mean_std.index)
-title="E vs Subfeature",
+title = "E vs Subfeature",
 fig = plt.figure()
-# fig.suptitle(title, fontsize=20)
-# ax = fig.add_subplot("{}{}{}".format(len(x), 1, idx + 1))
-# # ax.set_title("Subfeature {}".format(val), fontsize=16)
 plt.plot(df_mean_std.index, df_mean_std["mean"], label="mean")
 plt.plot(df_mean_std.index, df_mean_std["std"], label="std")
 plt.legend()
-# sns.boxplot(x=df_mean_std[], y=df_mean_std[y[idx]], ax=ax)
-# # ax.grid(color='k', linestyle='-', linewidth=0.5)
-
-
 plt.show()
-# print(mean, std)
-
